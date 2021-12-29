bfa4ec979720c0e06dff4c5c47b8c999970d3590
ghimiredipesh <ghimiredipesh2020@gmail.com>
2021-12-29 12:21:28 +0545

    simple calculator ui

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Calculator App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Simple Calculator"),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Enter First Number"),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Enter Second Number"),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(onPressed: () {}, child: Text("Add")),
+                  ElevatedButton(onPressed: () {}, child: Text("Substract")),
+                  ElevatedButton(onPressed: () {}, child: Text("Multiply")),
+                  ElevatedButton(onPressed: () {}, child: Text("Division"))
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 40,
+                width: double.infinity,
+                color: Colors.yellow,
+                child: Center(child: Text("Result: ")),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
